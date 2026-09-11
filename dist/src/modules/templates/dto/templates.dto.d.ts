b2e8e1c29ee81adcc4fff25b2b91a8bd700f7d72
infsolution <clsinfsolution@gmail.com>
2026-09-11 17:45:36 -0300

    remove dist

--- dist/src/modules/templates/dto/templates.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateTemplateDto {
-    name: string;
-    language: string;
-    category: string;
-    components_format?: string;
-}
-export declare class ListTemplatesQuery {
-    fields?: string;
-}
--- dist/src/modules/webhooks/webhooks.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-import { WebhooksService } from './webhooks.service';
-export declare class WebhooksController {
-    private readonly webhooksService;
-    constructor(webhooksService: WebhooksService);
-    verify(req: Request, mode: string, verifyToken: string, challenge: string, res: Response): void;
-    receive(req: Request, res: Response): Promise<void>;
-}
